83044cf8404057a90d759b304d12bc57de224c67
ezzAboElmakarem <ezzaboelmakarem@gmail.com>
2024-03-12 13:09:33 +0200

    convet unuseful stateful to stateless widget

--- lib/features/onbording/presentaion/views/onbording_screen_view.dart
+++ lib/features/onbording/presentaion/views/onbording_screen_view.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:kian_sheeps_projects/features/onbording/presentaion/views/widgets/onbording_screen_body.dart';
 
-class OnBordingScreenView extends StatefulWidget {
+class OnBordingScreenView extends StatelessWidget {
   const OnBordingScreenView({super.key});
 
-  @override
-  State<OnBordingScreenView> createState() => _OnBordingScreenViewState();
-}
-
-class _OnBordingScreenViewState extends State<OnBordingScreenView> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(

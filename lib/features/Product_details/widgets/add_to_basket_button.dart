24f05769028afdfb6f09204baf97ba5871e7954d
ezzAboElmakarem <ezzaboelmakarem@gmail.com>
2024-03-20 11:09:22 +0200

    edit add to cart button

--- lib/features/Product_details/widgets/add_to_basket_button.dart
+++ lib/features/Product_details/widgets/add_to_basket_button.dart
@@ -14,45 +14,40 @@ class AddToBasketButton extends StatelessWidget {
   final int price;
   @override
   Widget build(BuildContext context) {
-    return Positioned(
-      bottom: 6.h,
-      left: 6.w,
-      right: 6.w,
-      child: GestureDetector(
-        onTap: () {},
-        child: Container(
-          height: 66.h,
-          width: 324.w,
-          decoration: BoxDecoration(
-            color: kPrimaryColor,
-            borderRadius: BorderRadius.circular(16),
-          ),
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 16.h),
-            child: Row(
-              children: [
-                Text(
-                  '$price ر.س',
-                  textDirection: TextDirection.rtl,
-                  style: TextStyles.textstyle16.copyWith(color: Colors.white),
-                ),
-                const Spacer(),
-                Text(
-                  'اضف للسله',
-                  textDirection: TextDirection.rtl,
-                  style: TextStyles.textstyle16.copyWith(color: Colors.white),
-                ),
-                const SizedBox(
-                  width: 10,
-                ),
-                GestureDetector(
-                    onTap: () {
-                      navigateTo(context: context, widget: const CartView());
-                    },
-                    child: Image.asset(AssetsData.shoppingBasket,
-                        color: Colors.white)),
-              ],
-            ),
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        height: 66.h,
+        width: 324.w,
+        decoration: BoxDecoration(
+          color: kPrimaryColor,
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 16.h),
+          child: Row(
+            children: [
+              Text(
+                '$price ر.س',
+                textDirection: TextDirection.rtl,
+                style: TextStyles.textstyle16.copyWith(color: Colors.white),
+              ),
+              const Spacer(),
+              Text(
+                'اضف للسله',
+                textDirection: TextDirection.rtl,
+                style: TextStyles.textstyle16.copyWith(color: Colors.white),
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              GestureDetector(
+                  onTap: () {
+                    navigateTo(context: context, widget: const CartView());
+                  },
+                  child: Image.asset(AssetsData.shoppingBasket,
+                      color: Colors.white)),
+            ],
           ),
         ),
       ),

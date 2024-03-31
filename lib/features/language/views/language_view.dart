00cfda1b23af391128c5b875c9252af072715882
ezzAboElmakarem <ezzaboelmakarem@gmail.com>
2024-03-31 12:39:43 +0200

    QC : feat (Auth) : Fix localization Issues.

--- lib/features/language/views/language_view.dart
+++ lib/features/language/views/language_view.dart
@@ -2,6 +2,7 @@ import 'dart:developer';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:kian_sheeps_projects/helper/color_styles.dart';
 import 'package:kian_sheeps_projects/helper/routes.dart';
 import '../../../helper/app_bar_method.dart';
@@ -10,7 +11,7 @@ import '../../../widgets/custom_button.dart';
 
 class LanguageView extends StatefulWidget {
   const LanguageView({super.key, required this.appState});
-  final AppState appState;
+  final AppLocaleLang appState;
 
   @override
   State<LanguageView> createState() => _LanguageViewState();
@@ -20,9 +21,13 @@ class _LanguageViewState extends State<LanguageView> {
   late String _selectedLocale;
   // ValueNotifier<int> selectedValue = ValueNotifier<int>(0);
 
+  late final GetStorage _storage;
+
   @override
   void initState() {
-    _selectedLocale = RouteUtils.isAR ? "ar" : "en";
+    _storage = GetStorage();
+    _selectedLocale =
+        _storage.read('selectedLocale') ?? (RouteUtils.isAR ? "ar" : "en");
     super.initState();
   }
 
@@ -46,6 +51,7 @@ class _LanguageViewState extends State<LanguageView> {
               onChanged: (value) {
                 setState(() {
                   _selectedLocale = 'ar';
+                  _storage.write('selectedLocale', 'ar');
                 });
               },
             ),
@@ -57,6 +63,7 @@ class _LanguageViewState extends State<LanguageView> {
               onChanged: (value) {
                 setState(() {
                   _selectedLocale = 'en';
+                  _storage.write('selectedLocale', 'en');
                 });
               },
             ),
